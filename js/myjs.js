09b201b874662747c113fa29377d9360e0666fe3
Yakushin103 <VYakushin103@yandex.ru>
2019-02-24 09:53:49 +0300

    Сделал первые поправки

--- js/myjs.js
+++ js/myjs.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   // высота 100%
 // $('.header').height($(window).height());
 //прокрутка до выброного в меню блока
- $(".time a").click(function(){
+ $(".time__content a").click(function(){
  	$("body,html").animate({
  		scrollTop:$("#" + $(this).data('value')).offset().top
  	},1000)
